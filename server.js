faad226e185dbec1287a752fde4bd1c627a56a83
asiwajudev <olanrewajuranda@gmail.com>
2020-04-12 00:53:34 +0100

    MongoDB connected

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+//Import Model
+const employee = require("./Model/Employee");
 
 const app = express();
+
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-kya97.mongodb.net/test`;
+
+//Connect DB
+mongoose
+  .connect(mongoUri, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.get("/", (req, res) => {
   res.send("Welcome to Employee App Server!");
